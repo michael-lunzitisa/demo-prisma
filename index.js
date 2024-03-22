30f8438369c979189aa86664a7340c9542243917
Michael <mickaellunzola@gmail.com>
2024-03-22 12:09:06 +0100

    Install prisema

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require("express");
-const PORT = 3002;
+const PORT = process.env.PORT || 3002;
 const app = express();
 
 app.get("/", (req, res) => {
-    console.log("L'application fonctionne");
-    res.send("L'application fonctionne");
+    return res.send("L'application fonctionne");
 });
 
-app.listen(PORT, () => {
-    console.log(`Le serveur écoute sur le port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Le serveur écoute sur le port ${PORT}`));
